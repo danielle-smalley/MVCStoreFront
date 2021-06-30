929c2fe6905bf0ef8eaaaab40200a76ba48efa84
Danielle Smalley <smalley.danielle@gmail.com>
2021-06-30 15:04:16 -0500

    Identity Samples complete. Skipping template conversion to add the data layer

--- MVCStoreFront.UI/Startup.cs
+++ MVCStoreFront.UI/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(MVCStoreFront.UI.Startup))]
 namespace MVCStoreFront.UI
 {
     public partial class Startup
