a445b4538ef8e58bec5b88a6ac96422224d923b4
Danielle Smalley <smalley.danielle@gmail.com>
2021-06-30 10:40:41 -0500

    Initial Commit

---
+++ MVCStoreFront.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCStoreFront.UI.Startup))]
+namespace MVCStoreFront.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
